1c7e1347db658bfb1ce0731dea85142d3efcce44
ZeQ61 <zeki14@icloud.com>
2026-04-08 12:35:37 +0300

    hata 2

--- src/config/config.js
+++ src/config/config.js
@@ -11,7 +11,7 @@ export const config = {
     defaultZoom: 11
   },
 
-  // API Configuration
+  // API Configuration asd
   api: {
     baseUrl: 'https://bingolkart.com.tr/v1/api',
     timeout: 30000
